8cf9313acf13c8ef3ababa3a07a9f7d176c6cbb7
SebitaMiranda <seba.miranda1980@gmail.com>
2023-08-22 21:18:14 -0400

    para revision

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :movies
+  resources :clients
+
+  delete '/clients', to: 'clients#destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "movies#index"
+end
